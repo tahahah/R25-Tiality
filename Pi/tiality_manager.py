3587e88e650f7e292668d40d793a1540cd2ed23a
Ryan Hartshorne <ryan.ninjamaster@gmail.com>
2025-09-13 16:54:40 +1000

    Removed caches and testing new import strategy

--- Pi/tiality_manager.py
+++ Pi/tiality_manager.py
@@ -1,10 +1,16 @@
-import tiality_server
+import sys
+import os
 import queue
 import threading
 import argparse
 import time
 # from command import pi_command_manager_worker
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import tiality_server
+original_cwd = os.getcwd()
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 from video import pi_video_manager_worker, capture_frame_as_bytes, frame_generator_picamera2
+
 
 def main():
     parser = argparse.ArgumentParser(description="Pi Tiality Manager")
