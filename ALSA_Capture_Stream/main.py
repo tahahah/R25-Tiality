1339467535b2f35b07b1eafe564d8a315f145527
Odette Maher <do.dot.dodette@gmail.com>
2025-10-15 22:36:51 +1100

    Fix: Eliminate directional audio to reduce execution time

--- ALSA_Capture_Stream/main.py
+++ ALSA_Capture_Stream/main.py
@@ -121,43 +121,48 @@ if args.stream:
             direction_start_time = time()
 
             # Calculate direction (amplitude method)
-            amplitude_array = np.sqrt(np.mean(capture_buffer.astype(np.int32)**2, axis=0))
-            loudest_index = np.argsort(amplitude_array)[::-1]
-            if (settings.captured_channels == 4):
-                direction_amp_buffer[buffer_pointer] = np.atan2(amplitude_array[1]-amplitude_array[2],amplitude_array[3]-amplitude_array[0])-(np.pi/4)-(np.pi/2)
-                direction_amp_buffer[buffer_pointer] *= (180/np.pi)
-                if direction_amp_buffer[buffer_pointer] < -180: direction_amp_buffer[buffer_pointer] += 360
-            else:
-                direction_amp_buffer[buffer_pointer] = 90 if (loudest_index[0] == 0) else -90
+            # amplitude_array = np.sqrt(np.mean(capture_buffer.astype(np.int32)**2, axis=0))
+            # loudest_index = np.argsort(amplitude_array)[::-1]
+            # if (settings.captured_channels == 4):
+            #     direction_amp_buffer[buffer_pointer] = np.atan2(amplitude_array[1]-amplitude_array[2],amplitude_array[3]-amplitude_array[0])-(np.pi/4)-(np.pi/2)
+            #     direction_amp_buffer[buffer_pointer] *= (180/np.pi)
+            #     if direction_amp_buffer[buffer_pointer] < -180: direction_amp_buffer[buffer_pointer] += 360
+            # else:
+            #     direction_amp_buffer[buffer_pointer] = 90 if (loudest_index[0] == 0) else -90
 
             # Calculate direction (samples method)
-            for i in range(1,settings.captured_channels):
-                correlation_array[:,i-1] = np.correlate(capture_buffer[:,0],capture_buffer[:,i], 'full')
-            delay_array = np.argmax(correlation_array[max_lag_idx,:], axis=0) - max_lag
-            if (settings.captured_channels == 4):
-                # Account for the distance between microphones in the four-mic system
-                delay_array[0] = 2*delay_array[0]
-                delay_array[2] += (delay_array[2]-delay_array[1])
-                # Calculate the angle
-                direction_time_buffer[buffer_pointer] = np.atan2(delay_array[0]-delay_array[1],delay_array[2])-(np.pi/4)-(np.pi/2)
-                direction_time_buffer[buffer_pointer] *= (180/np.pi)
-                if direction_time_buffer[buffer_pointer] < -180: direction_time_buffer[buffer_pointer] += 360
-            else:
-                direction_time_buffer[buffer_pointer] = 90 if (delay_array < 0) else -90
+            # for i in range(1,settings.captured_channels):
+            #     correlation_array[:,i-1] = np.correlate(capture_buffer[:,0],capture_buffer[:,i], 'full')
+            # delay_array = np.argmax(correlation_array[max_lag_idx,:], axis=0) - max_lag
+            # if (settings.captured_channels == 4):
+            #     # Account for the distance between microphones in the four-mic system
+            #     delay_array[0] = 2*delay_array[0]
+            #     delay_array[2] += (delay_array[2]-delay_array[1])
+            #     # Calculate the angle
+            #     direction_time_buffer[buffer_pointer] = np.atan2(delay_array[0]-delay_array[1],delay_array[2])-(np.pi/4)-(np.pi/2)
+            #     direction_time_buffer[buffer_pointer] *= (180/np.pi)
+            #     if direction_time_buffer[buffer_pointer] < -180: direction_time_buffer[buffer_pointer] += 360
+            # else:
+            #     direction_time_buffer[buffer_pointer] = 90 if (delay_array < 0) else -90
             
             # Calculate overall amplitude
-            sig_amplitude_buffer[buffer_pointer] = np.mean(amplitude_array)
+            # sig_amplitude_buffer[buffer_pointer] = np.mean(amplitude_array)
 
-            buffer_pointer += 1
-            if buffer_pointer >= 50: buffer_pointer = 0
+            # buffer_pointer += 1
+            # if buffer_pointer >= 50: buffer_pointer = 0
 
 
             # Add information to header
-            header["direction"] = {"instantaneous_amp": direction_amp_buffer[buffer_pointer],
-                                "instantaneous_time": direction_time_buffer[buffer_pointer],
-                                "buffered_amp": np.median(direction_amp_buffer),
-                                "buffered_time": np.median(direction_time_buffer)}
-            header["amplitude"] = np.mean(sig_amplitude_buffer)
+            # header["direction"] = {"instantaneous_amp": direction_amp_buffer[buffer_pointer],
+            #                     "instantaneous_time": direction_time_buffer[buffer_pointer],
+            #                     "buffered_amp": np.median(direction_amp_buffer),
+            #                     "buffered_time": np.median(direction_time_buffer)}
+            # header["amplitude"] = np.mean(sig_amplitude_buffer)
+            header["direction"] = {"instantaneous_amp": 0.0,
+                                "instantaneous_time": 0.0,
+                                "buffered_amp": 0.0,
+                                "buffered_time": 0.0}
+            header["amplitude"] = 0.0
 
             # Stop direction time
             direction_duration = time() - direction_start_time
@@ -172,14 +177,14 @@ if args.stream:
                 print(f"Warning: Processing time {packet_duration:.3f}s > frame duration {settings.frame_duration}s")
             
             # Log stats periodically
-            if header["sequence_number"] % 100 == 0:
-                stats = udp_sender.get_stats()
-                print(f"Sent: {stats['packets_sent']} packets, {stats['bytes_sent']} bytes")
-                print("Whole packet duration: {:.1f} ms".format(packet_duration*1000))
-                print("Encoding duration: {:.1f} ms".format(encoding_duration*1000))
-                print("Direction duration: {:.1f} ms".format(direction_duration*1000))
-                print("Buffered direction: (amplitude) {:0.1f} deg (samples) {:0.1f} deg".format(np.median(direction_amp_buffer), np.median(direction_time_buffer)))
-                print("Buffered amplitude: {:0.1f}".format(np.mean(sig_amplitude_buffer)))
+            # if header["sequence_number"] % 100 == 0:
+            #     stats = udp_sender.get_stats()
+            #     print(f"Sent: {stats['packets_sent']} packets, {stats['bytes_sent']} bytes")
+            #     print("Whole packet duration: {:.1f} ms".format(packet_duration*1000))
+            #     print("Encoding duration: {:.1f} ms".format(encoding_duration*1000))
+            #     print("Direction duration: {:.1f} ms".format(direction_duration*1000))
+            #     print("Buffered direction: (amplitude) {:0.1f} deg (samples) {:0.1f} deg".format(np.median(direction_amp_buffer), np.median(direction_time_buffer)))
+            #     print("Buffered amplitude: {:0.1f}".format(np.mean(sig_amplitude_buffer)))
 
     except KeyboardInterrupt:
         print("\nStopping...")
